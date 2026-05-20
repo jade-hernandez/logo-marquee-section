5d60c982aee8cf82db99e37e7f7660ef6eed89d2
Jade Hernandez <jade.hernandez88@gmail.com>
2026-05-20 16:32:59 +0200

    refactor(ui): Refactors logo marquee into reusable component

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,12 @@ import robinwoodLogo from "./images/robinwood-logo.webp";
 import swapDoLogo from "./images/swapdo-logo.webp";
 import wanNainLogo from "./images/wan-nain-logo.webp";
 
-type Logo = {
+type LogoProps = {
   src: string;
   alt: string;
 };
 
-const logos: Logo[] = [
+const logos: LogoProps[] = [
   { src: wanNainLogo, alt: "Wan Nain" },
   { src: robinwoodLogo, alt: "Robinwood" },
   { src: swapDoLogo, alt: "SwapDo" },
@@ -18,6 +18,32 @@ const logos: Logo[] = [
   { src: airCarLogo, alt: "Air Car" },
   { src: makroHardLogo, alt: "Makro Hard" }
 ];
+
+type LogoStripeProps = {
+  "aria-hidden"?: boolean;
+};
+
+function LogoStripe({ "aria-hidden": ariaHidden }: LogoStripeProps) {
+  return (
+    <div
+      aria-hidden={ariaHidden}
+      className='animate-marquee flex shrink-0 flex-row gap-8'
+    >
+      {logos.map(logo => (
+        <div
+          key={logo.alt}
+          className='p-6'
+        >
+          <img
+            src={logo.src}
+            alt={logo.alt}
+            className='h-12 w-auto'
+          />
+        </div>
+      ))}
+    </div>
+  );
+}
 
 function App() {
   return (
@@ -27,57 +53,17 @@ function App() {
     >
       <div className='flex w-full flex-1 flex-col items-center justify-center rounded-md bg-white'>
         <main className='flex w-full flex-col items-center gap-8'>
-          <h1 className='text-base text-neutral-600'>Used by teams that you love</h1>
+          <h1
+            id='marquee-heading'
+            className='text-base text-neutral-600'
+          >
+            Used by teams that you love
+          </h1>
           {/* logo marquee */}
-          <div className='selection-none flex w-full overflow-hidden'>
-            <div className='animate-marquee flex shrink-0 flex-row gap-8'>
-              {logos.map(logo => (
-                <div
-                  key={logo.alt}
-                  className='p-6'
-                >
-                  <img
-                    src={logo.src}
-                    alt={logo.alt}
-                    className='h-12 w-auto'
-                  />
-                </div>
-              ))}
-            </div>
-            <div
-              className='animate-marquee flex shrink-0 flex-row gap-8'
-              aria-hidden='true'
-            >
-              {logos.map(logo => (
-                <div
-                  key={logo.alt}
-                  className='p-6'
-                >
-                  <img
-                    src={logo.src}
-                    alt={logo.alt}
-                    className='h-12 w-auto'
-                  />
-                </div>
-              ))}
-            </div>
-            <div
-              className='animate-marquee flex shrink-0 flex-row gap-8'
-              aria-hidden='true'
-            >
-              {logos.map(logo => (
-                <div
-                  key={logo.alt}
-                  className='p-6'
-                >
-                  <img
-                    src={logo.src}
-                    alt={logo.alt}
-                    className='h-12 w-auto'
-                  />
-                </div>
-              ))}
-            </div>
+          <div className='flex w-full overflow-hidden select-none'>
+            <LogoStripe />
+            <LogoStripe aria-hidden />
+            <LogoStripe aria-hidden />
           </div>
         </main>
       </div>
